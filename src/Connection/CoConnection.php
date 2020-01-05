e9377e159c15f62cadd40deefad2dc151f938b30
gilgamesh-y <805655528@qq.com>
2020-01-05 20:43:39 +0800

    fix bugs

--- src/Connection/CoConnection.php
+++ src/Connection/CoConnection.php
@@ -33,13 +33,14 @@ class CoConnection extends Connection
         if ($res === false) {
             throw new \Exception('获取数据失败', $this->connection->errCode);
         }
+        $res = bin_to_str($res);
         $res_len_arr = explode("-", $res);
         $status_len = $res_len_arr[0];
         $code_len = $res_len_arr[1];
-        $res = substr($res, strlen($status_len) + strlen($code_len) + 2, -1);
-        $res = unpack("A{$status_len}status/A{$code_len}code/A*data", $res);
+        $res = substr($res, strlen($status_len) + strlen($code_len) + 2);
+        $res = unpack("A{$status_len}status/L{$code_len}code/A*data", $res);
         $res['data'] = json_decode($res['data'], true);
-        
+
         return $res;
     }
 }
